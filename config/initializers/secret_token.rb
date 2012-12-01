3fb2cd40cd83f1e1a0692fb0b44b3598f981bbb3
Jesse Naiman <jessenaiman@gmail.com>
2012-12-01 13:11:15 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voteit::Application.config.secret_token = 'df3eb7eea242051ef0ba394f5704aee4d24a1af88379be9b843e3e075950f78000a6c6e4bae214ada81adda8122fad5249e7a7186212bbe899d87b8400bbca3b'
